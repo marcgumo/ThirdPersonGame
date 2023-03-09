32dacbbf99eedd7a1921f4df450893dd4b40b760
marcgumo <58789378+marcgumo@users.noreply.github.com>
2023-03-09 12:42:53 +0100

    Blend tree: movimiento principal

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,6 +26,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float dashDuration = 0.6f;
     [SerializeField] private float dashForce = 8f;
 
+    [Header("Animation settings")]
+    [SerializeField] private float acceleration = 2.5f;
+    [SerializeField] private float deceleration = 5f;
+    float playerSpeed;
+
+    Animator anim;
+
+
     void Start()
     {
         playerState = MovementStates.Initial;
@@ -35,6 +43,8 @@ public class PlayerController : MonoBehaviour
         charControl = GetComponent<CharacterController>();
 
         Cursor.lockState = CursorLockMode.Locked;
+
+        anim = GetComponentInChildren<Animator>();
     }
 
     void Update()
@@ -169,9 +179,23 @@ public class PlayerController : MonoBehaviour
             moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
             charControl.Move(speedMove * Time.deltaTime * moveDir.normalized);
+
+            if (playerSpeed < 1)
+                playerSpeed += acceleration * Time.deltaTime;
+            else
+                playerSpeed = 1;
+        }
+        else
+        {
+            if (playerSpeed > 0)
+                playerSpeed -= deceleration * Time.deltaTime;
+            else
+                playerSpeed = 0;
         }
 
         ConstantGravity();
+
+        anim.SetFloat("speed", playerSpeed);
     }
 
     private void ConstantGravity()
